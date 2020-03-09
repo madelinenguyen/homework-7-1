0111770b1f941af54f5124137ed70d92acbf3577
madelinenguyen <maddynnguyen@gmail.com>
2020-03-09 13:51:46 -0700

    Add files via upload

---
+++ src/ArrayDictionaryTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayDictionaryTest {
+    @Test
+    public void demo() {
+        int testSize = 5;
+        ArrayDictionary dict = new ArrayDictionary(testSize);
+        assertTrue(dict.add(2, 82));
+        assertTrue(dict.add(4, 84));
+        assertTrue(dict.add(7, 87));
+        System.out.println(dict);
+    }
+
+    @Test
+    public void remove() {
+        // homework
+        assertTrue(false);  // place holder
+    }
+
+    @Test
+    public void contains() {
+        // homework
+        assertTrue(false);  // place holder
+    }
+}
