046880397a698227f776d39bd4490974b45f6d17
madelinenguyen <maddynnguyen@gmail.com>
2020-03-14 23:46:05 -0700

    finished problem 1

--- src/ArrayDictionary.java
+++ src/ArrayDictionary.java
@@ -1,5 +1,5 @@
 public class ArrayDictionary implements Dictionary {
-    private int capacity;
+    private int capacity; // size of KVEntry array
     private int count;
     private KVEntry[] entries;
 
@@ -28,14 +28,14 @@ public class ArrayDictionary implements Dictionary {
 
         // when there's no entry yet
         if (entries[hashedKey] == null) {
-            if (count == capacity) {
-                return false;
-            }
+            if (count == capacity) return false;
+            
+            // create new KVEntry at index in entries
             entries[hashedKey] = new KVEntry(key, value);
             count++;
             return true;
         }
-
+        
         KVEntry ptr = entries[hashedKey];
         KVEntry pNewNode = null;
         while (ptr != null) {
@@ -44,8 +44,8 @@ public class ArrayDictionary implements Dictionary {
                 ptr.value = value;
                 return true;
             }
-            pNewNode = ptr;
-            ptr = ptr.next;
+            pNewNode = ptr; // keep track of old entry
+            ptr = ptr.next; // go to next chained value if any
         }
 
         // add an entry to the end of the chain
@@ -55,17 +55,94 @@ public class ArrayDictionary implements Dictionary {
 
     // Delete the entry with the given key from the dictionary
     // Return true if an entry is deleted, false otherwise
+    /*
     @Override
     public boolean remove(int key) {
-        // homework
+        if (capacity<1) {
+        	return false;
+        }
+        if (key<0) {
+        	return false;
+        }
+        int hashedKey = hashFunction(key);
+        if (entries[hashedKey]==null) {
+        	return false;
+        }
+        
+        if (entries[hashedKey].key == key) {
+        	entries[hashedKey] = entries[hashedKey].next;
+        	return true;
+        }
+        
+        KVEntry prev = entries[hashedKey];
+        KVEntry curr = prev.next;
+        while (curr != null) {
+        	if (curr.key==key) {
+        		prev.next = curr.next;
+        		return true;
+        	}
+        	prev=curr;
+        	curr=curr.next;
+        }
         return false;
     }
+    */
+    
+    
+    @Override
+    public boolean remove(int key) {
+    	// create array index
+    	int hashedKey = hashFunction(key);
+    	
+    	// check capacity
+    	if (capacity<1) return false;
+    	// check that key is valid
+    	if (key<0) return false;
+    	if (entries[hashedKey]==null) return false;
+    	
+    	// check if first key is passed in key
+    	if (entries[hashedKey].key == key) {
+    		entries[hashedKey] = entries[hashedKey].next;
+    		return true;
+    	}
+    	
+    	KVEntry ptr = entries[hashedKey];
+    	KVEntry nextKey = ptr.next;
+    	
+    	while (nextKey != null) {
+    		// check if the key at ptr is equal to the key
+    		if (nextKey.key == key) {
+    			ptr.next = nextKey.next; // remove
+    			count--;
+    			return true;
+    		}
+    		ptr = nextKey;
+    		nextKey = nextKey.next;
+    	}
+    	return false;
+    }
+    
 
     // Return true when the dictionary contains an entry
     // with the key
     @Override
     public boolean contains(int key) {
-        // homework
+    	
+    	int hashedKey = hashFunction(key);
+    	// check capacity
+    	if (capacity<1) return false;
+    	if (key<0) return false;
+    	// if there's no entry yet
+    	if (entries[hashedKey] == null) return false;
+    	
+    	KVEntry ptr = entries[hashedKey];
+    	//KVEntry nextKey = ptr.next;
+    	while (ptr != null) {
+    		if (ptr.key == key) {
+    			return true;
+    		}
+    		ptr = ptr.next;
+    	}
         return false;
     }
 
--- src/ArrayDictionaryTest.java
+++ src/ArrayDictionaryTest.java
@@ -15,13 +15,122 @@ public class ArrayDictionaryTest {
 
     @Test
     public void remove() {
-        // homework
-        assertTrue(false);  // place holder
+    	//ArrayDictionary<Integer, Integer> dict = new ArrayDictionary<Integer, Integer>();
+    	ArrayDictionary dict = new ArrayDictionary(1);
+    	
+    	// empty dictionary<int, int>, contain returns false with key -1
+    	assertTrue(!dict.remove(-1));
+    	// empty dictionary<int, int>, contain returns false with key 0
+    	assertTrue(!dict.remove(0));
+    	// empty dictionary<int, int>, contain returns false with key 1
+    	assertTrue(!dict.remove(1));
+    	
+    	
+    	ArrayDictionary dict1 = new ArrayDictionary(1);
+    	dict1.add(0, 1);
+    	dict1.add(0, 0);
+    	dict1.add(0, 3);
+    	
+    	// dictionary<int, int>, capacity 1, init values {0:103}, key=2 returns false
+    	assertTrue(!dict1.remove(2));
+    	// dictionary<int, int>, capacity 1, init values {0:103}, key=0 returns true
+    	assertTrue(dict1.remove(0));
+    	
+    	
+    	ArrayDictionary dict2 = new ArrayDictionary(2);
+    	dict2.add(0, 1);
+    	dict2.add(0, 0);
+    	dict2.add(0, 3);
+    	dict2.add(1, 1);
+    	dict2.add(1, 0);
+    	dict2.add(1, 5);
+    	
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=0 returns true
+    	assertTrue(dict2.remove(0));
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=1 returns true
+    	assertTrue(dict2.remove(1));
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=2 returns false
+    	assertTrue(!dict2.remove(2));
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=3 returns false
+    	assertTrue(!dict2.remove(3));
+    	
+    	
+    	ArrayDictionary dict3 = new ArrayDictionary(3);
+    	dict3.add(0, 1);
+    	dict3.add(0, 0);
+    	dict3.add(0, 3);
+    	dict3.add(1, 1);
+    	dict3.add(1, 0);
+    	dict3.add(1, 5);
+    			
+    	// dictionary<int, int>, capacity 3, init values {0:103, 1:105}, key=3 returns false
+    	assertTrue(!dict3.remove(3));
+    	dict3.add(2, 2);
+    	dict3.add(2, 0);
+    	dict3.add(2, 6);
+    	
+        // dictionary<int, int>, capacity 3, init values {0:103, 1:105, 2:206}, key=1 returns true
+    	assertTrue(dict3.remove(1));
     }
 
     @Test
     public void contains() {
-        // homework
-        assertTrue(false);  // place holder
+    	
+    	//ArrayDictionary<Integer, Integer> dict = new ArrayDictionary<Integer, Integer>();
+    	ArrayDictionary dict = new ArrayDictionary(1);
+    	
+    	// empty dictionary<int, int>, contain returns false with key -1
+    	assertTrue(!dict.contains(-1));
+    	// empty dictionary<int, int>, contain returns false with key 0
+    	assertTrue(!dict.contains(0));
+    	// empty dictionary<int, int>, contain returns false with key 1
+    	assertTrue(!dict.contains(1));
+    	
+    	
+    	ArrayDictionary dict1 = new ArrayDictionary(1);
+    	dict1.add(0, 1);
+    	dict1.add(0, 0);
+    	dict1.add(0, 3);
+    	
+    	// dictionary<int, int>, capacity 1, init values {0:103}, key=2 returns false
+    	assertTrue(!dict1.contains(2));
+    	// dictionary<int, int>, capacity 1, init values {0:103}, key=0 returns true
+    	assertTrue(dict1.remove(0));
+    	
+    	
+    	ArrayDictionary dict2 = new ArrayDictionary(2);
+    	dict2.add(0, 1);
+    	dict2.add(0, 0);
+    	dict2.add(0, 3);
+    	dict2.add(1, 1);
+    	dict2.add(1, 0);
+    	dict2.add(1, 5);
+    	
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=0 returns true
+    	assertTrue(dict2.remove(0));
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=1 returns true
+    	assertTrue(dict2.remove(1));
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=2 returns false
+    	assertTrue(!dict2.remove(2));
+    	// dictionary<int, int>, capacity 2, init values {0:103, 1:105}, key=3 returns false
+    	assertTrue(!dict2.remove(3));
+    	
+    	
+    	ArrayDictionary dict3 = new ArrayDictionary(3);
+    	dict3.add(0, 1);
+    	dict3.add(0, 0);
+    	dict3.add(0, 3);
+    	dict3.add(1, 1);
+    	dict3.add(1, 0);
+    	dict3.add(1, 5);
+    			
+    	// dictionary<int, int>, capacity 3, init values {0:103, 1:105}, key=3 returns false
+    	assertTrue(!dict3.remove(3));
+    	dict3.add(2, 2);
+    	dict3.add(2, 0);
+    	dict3.add(2, 6);
+    	
+        // dictionary<int, int>, capacity 3, init values {0:103, 1:105, 2:206}, key=1 returns true
+    	assertTrue(dict3.remove(1));
     }
 }
